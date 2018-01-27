f522dcb802d8286599cca5603b8511ec207d7e29
Tyler Wickline <twick00@gmail.com>
2018-01-26 16:37:22 -0800

    Add logic and more styling to get site working.

--- js/scripts.js
+++ js/scripts.js
@@ -39,21 +39,68 @@ $(document).ready(function (event) {
 
 function main() {
     var order = [];
+    pizzaArr = [];
     buildButtons();
-    $("#start-order").click(function(event) {
+    $("#start-order").click(function (event) {
         order.push(startAnOrder());
     });
+    $("#done-button").click(function (event) { 
+        
+        pizzaArr.push(donePizza());
+        console.log(pizzaArr);
+    });
+    $("#order-button").click(function (event) {
+        if (pizzaArr[0] !== undefined) {
+            placeOrder(pizzaArr);
+        }
+        else {
+            alert("Oops, looks like we misplaced your order.");
+        }
+    });
 }
+
 function buildButtons() {
-    for(var i = 0; i < pizzaIngreds.toppings.length; i++) {
-        $("#button-bar").append(" <button class='sm-btn btn-primary toppings' id='toppings-button-"+i+"' type='button'>"+pizzaIngreds.toppings[i]+"</button>");
+    for (var i = 0; i < pizzaIngreds.toppings.length; i++) {
+        $("#button-bar").append("<label class='btn btn-primary toppings active' ><input type='checkbox' class='toppings' name='"+pizzaIngreds.toppings[i]+"' autocomplete='off'>" + pizzaIngreds.toppings[i] + "</label>");
     }
-    for(var i= 0;i< pizzaIngreds.crusts.length; i++) {
-        $("#button-bar").append(" <button class='sm-btn btn-primary crusts' id='crusts-button-"+i+"' type='button'>"+pizzaIngreds.crusts[i]+"</button>");
+    for (var i = 0; i < pizzaIngreds.crusts.length; i++) {
+        $("#button-bar").append("<label class='btn btn-primary crusts active'><input type='checkbox' class='crusts' name='"+pizzaIngreds.crusts[i]+"'autocomplete='off'>" + pizzaIngreds.crusts[i] + "</label>");
     }
-    for(var i = 0;i < pizzaIngreds.cheese.length;i++) {
-        $("#button-bar").append(" <button class='sm-btn btn-primary cheese' id='cheese-button-"+i+"' type='button'>"+pizzaIngreds.cheese[i]+"</button>");
+    for (var i = 0; i < pizzaIngreds.cheese.length; i++) {
+        $("#button-bar").append("<label class='btn btn-primary cheese active'><input type='checkbox' class='cheese' name='"+pizzaIngreds.cheese[i]+"'autocomplete='off'>" + pizzaIngreds.cheese[i] + "</label>");
     }
+}
+function placeOrder(pizzaArr) {
+    console.log(pizzaArr);
+    for(var i = 0; i < pizzaArr.length; i++) {
+        $(".pizza-output").append('<br><h5>Pizza '+(i+1)+'</h5>');
+        $(".pizza-output").append("<div class='col' id='crusts-output'>");
+        $(".pizza-output").append("<p><strong>Crusts:</strong></p>");
+        $(".pizza-output").append("<ol>");
+        for(var o = 0; o < pizzaArr[i].crust.length; o++){
+            $(".pizza-output").append("<li>"+pizzaArr[i].crust[o]+"</li>");
+        }
+        //$(".pizza-output").append("</ol>");
+        $(".pizza-output").append("</div");
+        $(".pizza-output").append("<div class='col' id='cheese-output'>");
+        $(".pizza-output").append("<p><strong>Cheese:</strong></p>");
+        $(".pizza-output").append("<ol>");
+        for(var u = 0; u < pizzaArr[i].cheese.length; u++){
+            $(".pizza-output").append("<li>"+pizzaArr[i].cheese[u]+"</li>");
+        }
+        //$(".pizza-output").append("</ol>");
+        $(".pizza-output").append("</div");
+        $(".pizza-output").append("<div class='col' id='topping-output'>");
+        $(".pizza-output").append("<p><strong>Toppings:</strong></p>");
+        $(".pizza-output").append("<ol>");
+        for(var y = 0; y < pizzaArr[i].toppings.length; y++){
+            $(".pizza-output").append("<li>"+pizzaArr[i].toppings[y]+"</li>");
+        }
+        //$(".pizza-output").append("</ol>");
+        $(".pizza-output").append("</div");
+    }
+    $(".output").show();
+    $(".input").hide();
 }
 
 var pizzaIngreds = {
@@ -71,69 +118,106 @@ var pizzaIngreds = {
     crusts: [
         "Pan Crust", "Thin Crust", "Hand Tossed Crust", "Stuffed Crust"
     ],
-    cheese: ["Mozzarella","Cheddar","Mixed",]
+    cheese: ["Mozzarella", "Cheddar", "Mixed", ]
 };
+function donePizza() {
+    var toppings = $('.toppings:checkbox:checked').map(function() {
+        $(this).prop('checked', false);
+        return this.name;
+    }).get();
+    var crusts = $('.crusts:checkbox:checked').map(function() {
+        $(this).prop('checked', false);
+        return this.name;
+    }).get();
+    var cheese = $('.cheese:checkbox:checked').map(function() {
+        $(this).prop('checked', false);
+        return this.name;
+    }).get();
+    if ((toppings[0] === undefined) || (crusts[0] === undefined) || (cheese[0] === undefined)){
+        alert("You're missing some ingredients.");
+        return;
+    }
+    //console.log(toppings, crusts, cheese);
+    var pizza = new Pizza("Medium",crusts,cheese,toppings);
+    $(".button-row").hide();
+    $("#back-button").hide();
+    $("#next-button").hide();
+    $("#done-button").hide();
+    $("#start-order").prop("disabled", false);
+    $("#next-button").prop("disabled", false);
+    $("#order-button").show();
+    return pizza;
+}
+
+
+
+
 function startAnOrder() {
+    $(".button-row").show();
+    $("#start-order").prop("disabled", true);
     var index = 0;
     $(".toppings").show();
+    $(".crusts").hide();
+    $(".cheese").hide();
     $("#back-button").show();
     $("#next-button").show();
-    $("#back-button").prop("disabled",true);
-    $("#next-button").click(function(event) {
-        switch(index) {
+    $("#next-button").prop("disabled", false);
+    $("#back-button").prop("disabled", true);
+    $("#next-button").click(function (event) {
+        switch (index) {
             case 0:
-            index = 1;
-            $(".toppings").hide();
-            $(".crusts").show();
-            $(".cheese").hide();
-            $("#back-button").prop("disabled",false);
-            $("#next-button").prop("disabled",false);
-            break;
+                index = 1;
+                $(".toppings").hide();
+                $(".crusts").show();
+                $(".cheese").hide();
+                $("#back-button").prop("disabled", false);
+                $("#next-button").prop("disabled", false);
+                break;
             case 1:
-            index = 2;
-            $(".toppings").hide();
-            $(".crusts").hide();
-            $(".cheese").show();
-            $("#back-button").prop("disabled",false);
-            $("#next-button").prop("disabled",true);
-            break;
+                index = 2;
+                $(".toppings").hide();
+                $(".crusts").hide();
+                $(".cheese").show();
+                $("#back-button").prop("disabled", false);
+                $("#next-button").prop("disabled", true);
+                $("#done-button").show();
+                break;
             default:
-            index = 0;
-            console.log("Index was outside of scope: "+index);
-            break;
+                index = 0;
+                break;
         }
     });
-    $("#back-button").click(function(event) {
-        switch(index) {
+    $("#back-button").click(function (event) {
+        switch (index) {
             case 1:
-            index = 0;
-            $(".toppings").show();
-            $(".crusts").hide();
-            $(".cheese").hide();
-            $("#back-button").prop("disabled",true);
-            $("#next-button").prop("disabled",false);
-            break;
+                index = 0;
+                $(".toppings").show();
+                $(".crusts").hide();
+                $(".cheese").hide();
+                $("#back-button").prop("disabled", true);
+                $("#next-button").prop("disabled", false);
+                break;
             case 2:
-            index = 1;
-            $(".toppings").hide();
-            $(".crusts").show();
-            $(".cheese").hide();
-            $("#back-button").prop("disabled",false);
-            $("#next-button").prop("disabled",false);
-            break;
+                index = 1;
+                $(".toppings").hide();
+                $(".crusts").show();
+                $(".cheese").hide();
+                $("#back-button").prop("disabled", false);
+                $("#next-button").prop("disabled", false);
+                $("#done-button").hide();
+                break;
             case 3:
-            index = 2;
-            $(".toppings").hide();
-            $(".crusts").hide();
-            $(".cheese").show();
-            $("#back-button").prop("disabled",false);
-            $("#next-button").prop("disabled",true);
-            break;
+                index = 2;
+                $(".toppings").hide();
+                $(".crusts").hide();
+                $(".cheese").show();
+                $("#back-button").prop("disabled", false);
+                $("#next-button").prop("disabled", true);
+                break;
             default:
-            index = 0;
-            console.log("Index was outside of scope: "+index)
-            break;
+                index = 0;
+                break;
         }
-        
+
     });
 }
